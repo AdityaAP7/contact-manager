8ea30d4219974df2d6112461b87a0a351dc8f44f
AdityaAP7 <Adityapalodkar@gmail.com>
2024-07-20 19:18:56 +0530

    Added the routes

---
+++ routes/contactRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router();
+
+router.route('/').get((req,resp)=>{
+    resp.status(200).json({message:"hiiiii"})
+})
+
+
+router.route('/').post((req,resp)=>{
+    resp.status(200).json({message:"post called"})
+})
+
+router.route('/:id').get((req,resp)=>{
+    resp.status(200).json({message:"particular contact"})
+})
+
+router.route('/:id').put((req,resp)=>{
+    resp.status(200).json({message:`Updated the contact for ${req.params.id}`})
+})
+
+router.route('/:id').delete((req,resp)=>{
+    resp.status(200).json({message:`${req.params.id} deleted the contact`})
+})
+
+
+
+module.exports = router;
