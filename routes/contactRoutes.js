2b2dacf579853a413acd7d1ea7c516fd518d59c6
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-07-02 23:30:53 +0530

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,4 +1,4 @@
-765432187654321wwwwwwwwwwqqqqqqqqqqqconst express = require("express")
+8765432187654321wwwwwwwwwwqqqqqqqqqqqconst express = require("express")
 const router = express.Router();
 
 router.route('/').get((req,resp)=>{
